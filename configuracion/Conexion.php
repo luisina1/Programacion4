c673176a6293865bbb9fd8c1ca450841869361eb
luisina <luisinagomez752@gmail.com>
2024-10-12 14:45:26 -0300

    SECTOR CATEGORIA LISTO

---
+++ configuracion/Conexion.php
@@ -0,0 +1,35 @@
+<?php
+require_once "global.php";
+$conexion = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+mysqli_query($conexion, 'SET NAMES "'.DB_ENCODE.'"');
+
+if (mysqli_connect_errno()) {
+    printf("Falló la conexión a la base de datos: %s\n", mysqli_connect_error());
+    exit();
+}
+
+function ejecutarConsulta($sql) {
+    global $conexion;
+    $query = $conexion->query($sql);
+    return $query;
+}
+
+function ejecutarConsultaUnica($sql) {
+    global $conexion;
+    $query = $conexion->query($sql);
+    $row = $query->fetch_assoc();
+    return $row;
+}
+
+function ejecutarConsulta_retornarID($sql) {
+    global $conexion;
+    $query = $conexion->query($sql);
+    return $conexion->insert_id;
+}
+
+function limpiarCadena($str) { // Añadir el parámetro $str
+    global $conexion;
+    $str = mysqli_real_escape_string($conexion, trim($str));
+    return htmlspecialchars($str);
+}
+?>
---
+++ configuracion/global.php
@@ -0,0 +1,9 @@
+<?php
+define("DB_HOST","localhost");
+define("DB_NAME","sys");
+define("DB_USERNAME","root");
+define("DB_PASSWORD","root");
+define("DB_ENCODE","utf8"); // Corrección de "uft8" a "utf8"
+define("SISTEMA_NOMBRE","POS_SYSTEM");
+?>
+
